1c2e96aa7ad56d9049d53de1689d495db4bb2af8
Ivan Sharapenkov <ivansharapenkov@gmail.com>
2018-09-13 17:35:34 +0300

    Using getopt_long for argument parsing

--- utilities/vmcontrol/help/help.c
+++ utilities/vmcontrol/help/help.c
@@ -4,16 +4,16 @@
 void printHelp() {
 
     printf("Domain Management:\n");
-    printf("\t%-30s%-30s\n", "create", "Create a domain from an XML file");
-    printf("\t%-30s%-30s\n", "destroy", "Destroy (delete) a domain");
-    printf("\t%-30s%-30s\n", "resume", "Resume a domain");
-    printf("\t%-30s%-30s\n", "suspend", "Suspend a domain");
+    printf("\t%-20s%-30s%-30s\n", "--create", "required argument", "Create a domain from an XML file");
+    printf("\t%-20s%-30s%-30s\n", "--destroy", "required argument", "Destroy (delete) a domain");
+    printf("\t%-20s%-30s%-30s\n", "--resume", "required argument", "Resume a domain");
+    printf("\t%-20s%-30s%-30s\n", "--suspend", "required argument", "Suspend a domain");
 
     printf("\nDomain Monitoring:\n");
-    printf("\t%-30s%-30s\n", "list", "Show domains list");
+    printf("\t%-20s%-30s%-30s\n", "--list", "no argument", "Show domains list");
 
     printf("\nSupporting:\n");
-    printf("\t%-30s%-30s\n", "help", "Print help");
+    printf("\t%-20s%-30s%-30s\n", "--help", "no argument", "Print help");
 
     printf("\n\n");
 
--- utilities/vmcontrol/vmcontrol.c
+++ utilities/vmcontrol/vmcontrol.c
@@ -1,13 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 #include "vmcontrol.h"
 
 int main(int argc, char **argv)
 {
+    struct globalArgs_t {
+        char *xml_desc;
+    } globalArgs;
 
-    if(argc == 1) return 0;
+    static const char *optString = "c:d:s:r:lh?";
+    static const struct option longOpts[] = {
+        { "create", required_argument, NULL, 'c' },
+        { "destroy", required_argument, NULL, 'd' },
+        { "suspend", required_argument, NULL, 's' },
+        { "resume", required_argument, NULL, 'r' },
+        { "list", no_argument, NULL, 'l' },
+        { "help", no_argument, NULL, 'h' },
+        { NULL, no_argument, NULL, 0 }
+    };
+    
+    int longIndex;
+    int opt = getopt_long( argc, argv, optString, longOpts, &longIndex );
 
+    while( opt != -1 ) {
+        switch( opt ) {
+            case 'c':
+                globalArgs.xml_desc = readXml(optarg);
+                createAndRunDomain(globalArgs.xml_desc);
+                break;
+                 
+            case 'd':
+                destroyDomain(optarg);
+                break;
+                 
+            case 's':
+                suspendDomain(optarg);
+                break;
+                 
+            case 'r':
+                resumeDomain(optarg);
+                break;
+                 
+            case 'l':
+                showDomainList();
+                break;
+
+            case 'h':
+            case '?':
+                printHelp();
+                break;
+                 
+            default:
+                break;
+        }
+         
+        opt = getopt_long( argc, argv, optString, longOpts, &longIndex );
+    }
+/*
     if(!strcmp(argv[1], "create")) {
         
         if(argc == 3) {
@@ -44,7 +95,7 @@ int main(int argc, char **argv)
         printf("For more information use: vmcontrol help\n");
     
     }
-
+*/
     return 0;
 
 }
